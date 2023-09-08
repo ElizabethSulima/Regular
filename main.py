63df841be4d32f40b9b87cc76a9011250c1e11fd
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-09-08 12:31:04 +0700

    Main changed

--- main.py
+++ main.py
@@ -6,35 +6,38 @@ with open("phonebook_raw.csv", "r", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
     redact_list = []
-pprint(contacts_list)
+# pprint(contacts_list)
 
 def names_change():
     pattern_name = r'([А-Я])'
     new_name = r' \1'
-    for i in contacts_list[1:]:
-        line = i[0] + i[1] + i[2]
+    for element in contacts_list[1:]:
+        line = " ".join(element[:3])
         if len((re.sub(pattern_name, new_name, line).split())) == 3:
-            i[0] = re.sub(pattern_name, new_name, line).split()[0]
-            i[1] = re.sub(pattern_name, new_name, line).split()[1]
-            i[2] = re.sub(pattern_name, new_name, line).split()[2]
+            element[0] = re.sub(pattern_name, new_name, line).split()[0]
+            element[1] = re.sub(pattern_name, new_name, line).split()[1]
+            element[2] = re.sub(pattern_name, new_name, line).split()[2]
         elif len((re.sub(pattern_name, new_name, line).split())) == 2:
-            i[0] = re.sub(pattern_name, new_name, line).split()[0]
-            i[1] = re.sub(pattern_name, new_name, line).split()[1]
-            i[2] = ''
+            element[0] = re.sub(pattern_name, new_name, line).split()[0]
+            element[1] = re.sub(pattern_name, new_name, line).split()[1]
+            element[2] = ''
         elif len((re.sub(pattern_name, new_name, line).split())) == 1:
-            i[0] = re.sub(pattern_name, new_name, line).split()[0]
-            i[1] = ''
-            i[2] = ''
+            element[0] = re.sub(pattern_name, new_name, line).split()[0]
+            element[1] = ''
+            element[2] = ''
     return
+
 
 def formatting_phones():
     pattern_phone = re.compile(r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
     phone_sub = r'+7(\2)\3-\4-\5\7\8\9'
-    for i in contacts_list:
-        i[5] = pattern_phone.sub(phone_sub, i[5])
+    for element in contacts_list:
+        element[5] = pattern_phone.sub(phone_sub, element[5])
     return
 
 def editing_list():
+    names_list = []
+    info_list = []
     for i in contacts_list[1:]:
         first_name = i[0]
         last_name = i[1]
@@ -52,10 +55,11 @@ def editing_list():
                     i[5] = j[5]
                 if i[6] == '':
                     i[6] = j[6]
-    for n in contacts_list:
-        if n not in redact_list:
-            redact_list.append(n)
-    return redact_list
+    for sublist in contacts_list:
+        if sublist not in redact_list:
+            redact_list.append(sublist)
+    # pprint(redact_list)
+    return
 
 if __name__ == '__main__':
     names_change()
